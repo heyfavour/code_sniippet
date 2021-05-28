eb7bc301086ebd5ead85824e82ae308793cc0d1b
heyfavourite <619511821@qq.com>
2021-05-28 16:18:33 +0800

    先息后本利息计算-借呗

--- finance.py
+++ finance.py
@@ -1,5 +1,87 @@
-#收到请求
-#计提检查
-#还款试算
-#保存请求及试算结果 防止跨日等操作的进行二次试算导致的差异
-#还款--->同时用于轮询 保证接口唯一
+import datetime
+import math
+from decimal import *
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import *
+
+
+def repay():
+    pass
+    # 收到请求
+    # 幂等 检查表是否存在数据 存在则返回
+    # 计提检查
+    # 还款试算
+    # 保存请求及试算结果 防止跨日等操作的进行二次试算导致的差异
+    # 还款--->同时用于轮询 保证接口唯一
+
+
+def cal_repay_day(repay_day=None):
+    """
+    计算固定还款日
+    :param repay_day:
+    :return:
+    """
+    today = datetime.date.today()
+    today = datetime.datetime.strptime('20210518', '%Y%m%d')
+    if repay_day is None:  # 第一次贷款 1-28 29->25 30->26 31->27
+        a, b = divmod(today.day, 29)
+        if a == 0: repay_day = today.day
+        if a == 1: repay_day = today.day - 28 + 24  # 29号以后从25日开始还款
+        return f'{repay_day:0=2d}'
+    else:
+        return repay_day
+
+
+def cal_repay_dates(repay_day, terms, first_min_days=0):
+    """
+    结算每期还款日
+    :param repay_day: 固定还款日
+    :param first_min_days: 最小还款间隔日
+    :return:generator
+    """
+    today = datetime.date.today()
+    today = datetime.datetime.strptime('20210518', '%Y%m%d')
+    fist_term_date = datetime.datetime.strptime(
+        (today + relativedelta(months=1)).strftime('%Y%m') + repay_day, '%Y%m%d'
+    )
+    if (fist_term_date - today).days < first_min_days:  # 小于首期最短日期 延后一期
+        fist_term_date = fist_term_date + relativedelta(months=1)
+    repay_dates = rrule(MONTHLY, count=terms, dtstart=fist_term_date)
+    return repay_dates
+
+
+def repay_plan_trail(loan_amt, terms, cal_way, day_rate, first_min_days=15, repay_day='02'):
+    today = datetime.date.today()
+    today = datetime.datetime.strptime('20210518', '%Y%m%d')
+    repay_day = cal_repay_day(repay_day)
+    repay_dates = cal_repay_dates(repay_day, terms, first_min_days)
+
+    ratio = 1
+    repay_plan = []
+    last_cal_start_date = today
+    for repay_date in repay_dates:
+        interest_days = (repay_date - last_cal_start_date).days
+        ratio = ratio / (1 + day_rate * interest_days)
+        repay_plan.append([repay_date, ratio, interest_days])
+        last_cal_start_date = repay_date
+    term_amt = (loan_amt / sum([i[1] for i in repay_plan])).quantize(Decimal("0.00"), ROUND_DOWN)  # 向下取整
+    remain_prin_amt = loan_amt
+    for index, term in enumerate(repay_plan):
+        int_amt = (remain_prin_amt * day_rate * term[2]).quantize(Decimal("0.00"), ROUND_HALF_UP)  # 四舍五入
+        prin_amt = term_amt - int_amt
+        repay_plan[index].extend([prin_amt, int_amt, term_amt])
+        remain_prin_amt = remain_prin_amt - prin_amt  # 本金
+
+    if remain_prin_amt > 0:
+        repay_plan[-1][3] = repay_plan[-1][3] + remain_prin_amt
+        repay_plan[-1][5] = repay_plan[-1][5] + remain_prin_amt
+
+    return repay_plan
+
+
+if __name__ == '__main__':
+    loan_amt = 10000
+    day_rate = Decimal('0.00020')
+    terms = 36
+    cal_way = '等额本息'
+    repay_plan = repay_plan_trail(loan_amt=loan_amt, day_rate=day_rate, terms=terms, cal_way=cal_way)
--- round2.py
+++ round2.py
@@ -1,5 +1,15 @@
 import decimal
 #四舍五入
+"""
+ROUND_CEILING 总是趋向无穷大向上取整
+ROUND_DOWN　总是趋向0取整
+ROUND_FLOOR 总是趋向负无穷大向下取整
+ROUND_HALF_DOWN　如果最后一个有效数字大于或等于5则朝0反方向取整；否则，趋向0取整
+ROUND_HALF_EVEN　类似于ROUND_HALF_DOWN，不过，如果最后一个有效数字值为5，则会检查前一位。偶数值会导致结果向下取整，奇数值导致结果向上取整
+ROUND_HALF_UP 类似于ROUND_HALF_DOWN，不过如果最后一位有效数字为5，值会朝0的反方向取整
+ROUND_UP　朝0的反方向取整
+ROUND_05UP　如果最后一位是0或5，则朝0的反方向取整；否则向0取整
+"""
 context = decimal.getcontext()
 context.rounding = decimal.ROUND_HALF_UP
 print(round(decimal.Decimal(str(2.665)), 2))
