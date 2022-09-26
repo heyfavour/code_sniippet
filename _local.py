fed1cf37fdfba68e74a8f852ba4fc93e6e74827c
Mr.Wang <619511821@qq.com>
2022-09-26 23:19:00 +0800

    线程 协程 local

---
+++ _local.py
@@ -0,0 +1,52 @@
+import threading
+import contextvars, asyncio
+
+
+def thread_run_next():
+    for i in range(100):
+        local.num = local.num + 1
+    print(local.num)
+
+
+def thread_run():
+    local.num = 1
+    for i in range(100):
+        local.num = local.num + 1
+    thread_run_next()
+    print(local.num)
+
+
+def threading_run():
+    global local
+    local = threading.local()
+    t1 = threading.Thread(target=thread_run)
+    t2 = threading.Thread(target=thread_run)
+    t1.start()
+    t2.start()
+
+
+async def two(i):
+    # lock = contextvars.ContextVar("lock")
+    for i in range(100):
+        lock.set(lock.get() + 1)
+
+
+async def one(i):
+    # lock = contextvars.ContextVar("lock")
+    lock.set(i)
+    await two(i)
+    for i in range(100):
+        lock.set(lock.get() + 1)
+    print(lock.get())
+
+
+async def async_run():
+    global lock
+    lock = contextvars.ContextVar("lock")
+    tasks = [one(i) for i in range(5)]
+    await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    threading_run()
+    asyncio.run(async_run())
