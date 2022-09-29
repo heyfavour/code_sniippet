59af84175275c3ea29ac32f4bf3ab79d84a57644
heyfavourite <619511821@qq.com>
2022-09-29 17:38:25 +0800

    aiopika rpc

--- rabbitmq/_aiopika/rpc.py
+++ rabbitmq/_aiopika/rpc.py
@@ -1,93 +1,84 @@
-import time
+import json
+import os
+import asyncio
 
-import pika
+from aio_pika import DeliveryMode, ExchangeType, Message, connect_robust
+from aio_pika.abc import AbstractIncomingMessage
+from multiprocessing import Process
+
 import uuid
 
-"""
-Property
-
-Delivery mode: 是否持久化，1 - Non-persistent，2 - Persistent
-Headers：{'x-delay': data["delay-time"]}
-Properties: You can set other message properties here (delivery mode and headers are pulled out as the most common cases). Invalid properties will be ignored. Valid properties are:
-content_type ： 消息内容的类型  text/json
-content_encoding： 消息内容的编码格式
-priority： 消息的优先级
-correlation_id：关联id
-reply_to: 用于指定回复的队列的名称
-expiration： 消息的失效时间
-message_id： 消息id
-timestamp：消息的时间戳
-type： 类型
-user_id: 用户id
-app_id： 应用程序id
-cluster_id: 集群id
-Payload: 消息内容(必须是以字符串形式传入)
-"""
 HOST = "49.235.242.224"
 PORT = 50009
 
 
-def client():
-    credentials = pika.PlainCredentials("admin", "admin")
-    con_para = pika.ConnectionParameters(host=HOST, port=PORT, credentials=credentials)
-    connection = pika.BlockingConnection(con_para)
-    channel = connection.channel()
+async def client(i):
+    data = {"id": i, "status": "成功"}
 
-    queue = channel.queue_declare(queue='', exclusive=True)
-    client_queue = queue.method.queue
-    response = None
-    corr_id = str(uuid.uuid1())
+    connection = await connect_robust(host=HOST, port=PORT, login="admin", password="admin")  # ->Connection
+    channel = await connection.channel()  # publisher_confirms 交换机确认 默认True
+    rpc_ex = await channel.declare_exchange("rpc-ex", ExchangeType.DIRECT)
+    rpc_queue = await channel.declare_queue(exclusive=True)
+    await rpc_queue.bind(rpc_ex,routing_key="rpc")
 
-    def callback(ch, method, properties, body):
-        print(f'[Client] callback receive {body}')
-        nonlocal response
-        if corr_id == properties.correlation_id:
-            response = body
+    def callback(message: AbstractIncomingMessage) -> None:
+        if message.correlation_id !=correlation_id:
+            print(f"[C{i}]Bad message {message.body}")
+            return
+        print(f"[C{i}] Received message is: {json.loads(message.body)}")
 
-    channel.basic_consume(queue=client_queue, on_message_callback=callback, auto_ack=True)
-    channel.basic_publish(
-        exchange='',
-        routing_key='rpc_queue',
-        properties=pika.BasicProperties(reply_to=client_queue, correlation_id=corr_id),
-        body=str(30)
+    await rpc_queue.consume(callback)
+    correlation_id = str(uuid.uuid4())
+
+    await rpc_ex.publish(
+        Message(
+            json.dumps(data, ensure_ascii=False).encode("utf-8"),
+            correlation_id=correlation_id,
+            reply_to=rpc_queue.name,
+        ),
+        routing_key="rpc",
     )
-    while response is None:
-        connection.process_data_events()
-    print(f'[Client] Receive {response}')
+    await channel.close()
 
 
-def server():
-    credentials = pika.PlainCredentials("admin", "admin")
-    con_para = pika.ConnectionParameters(host=HOST, port=PORT, credentials=credentials)
-    connection = pika.BlockingConnection(con_para)
-    channel = connection.channel()
+async def server():
+    connection = await connect_robust(host=HOST, port=PORT, login="admin", password="admin")  # ->Connection
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=1)
 
-    queue = channel.queue_declare(queue='rpc_queue', exclusive=True)
+    rpc_ex = await channel.declare_exchange("rpc-ex", ExchangeType.DIRECT)
+    queue = await channel.declare_queue("rpc_queue")
+    await queue.bind(rpc_ex,routing_key="rpc")
 
-    channel.basic_qos(prefetch_count=1)
+    print("[S] Awaiting RPC requests")
 
-    def callback(ch, method, props, body):
-        print(f"[SERVER] receive {props.reply_to} {props.correlation_id}")
-        n = int(body)
-        response = int(n) + 1
-        ch.basic_publish(
-            exchange='',  # 使用默认交换机
-            routing_key=props.reply_to,  # response发送到该queue
-            properties=pika.BasicProperties(correlation_id=props.correlation_id),  # 使用correlation_id让此response与请求消息对应起来
-            body=str(response)
-        )
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+    async with queue.iterator() as qiterator:
+        message: AbstractIncomingMessage
+        async for message in qiterator:
+            async with message.process(requeue=False):
+                assert message.reply_to is not None
+                data = json.loads(message.body)
+                data["id"] = -1*data["id"]
+                await rpc_ex.publish(
+                    Message(
+                        json.dumps(data, ensure_ascii=False).encode("utf-8"),
+                        delivery_mode=DeliveryMode.PERSISTENT,
+                        correlation_id=message.correlation_id,
+                    ),
+                    routing_key=message.reply_to,
+                )
+def run_client():
+    for i in range(10):asyncio.run(client(i))
 
-    channel.basic_consume(queue='rpc_queue', on_message_callback=callback, auto_ack=False)
+def run_server():
+    asyncio.run(server())
 
-    print(" [x] Awaiting RPC requests")
-    channel.start_consuming()
-
-
-if __name__ == '__main__':
-    from multiprocessing import Process
-
-    s = Process(target=server)
-    c = Process(target=client)
+def run():
+    s = Process(target=run_server)
+    c = Process(target=run_client)
     s.start()
     c.start()
+
+if __name__ == '__main__':
+    run()
+
