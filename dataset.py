628994fa2aca19f0003203519bb834382333581a
Mr.Wang <619511821@qq.com>
2023-04-26 19:26:27 +0800

    dataset

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader, random_split
 
 class LearnDataset(Dataset):
     def __init__(self):
-        self.data = np.array([i for i in range(1000)]).reshape((-1, 2))
+        self.data = np.array([i for i in range(50)]).reshape((-1, 2))
         # print(self.data)
 
     def __len__(self):
@@ -15,23 +15,30 @@ class LearnDataset(Dataset):
     def __getitem__(self, index):
         idx_data = self.data[index]
         x = torch.FloatTensor(idx_data)
-        y = torch.Tensor(x[1])
+        y = torch.LongTensor([0])[0]
         return x, y  # [65] [1]
 
 
 def get_dataloader():
     dataset = LearnDataset()  #
-    train_num = int(0.7 * len(dataset))
+    train_num = int(0.6 * len(dataset))
     lengths = [train_num, len(dataset) - train_num]
     trainset, validset = random_split(dataset, lengths)
-    train_loader = DataLoader(trainset, batch_size=8, shuffle=True, drop_last=True)
-    valid_loader = DataLoader(validset, batch_size=12, shuffle=True, drop_last=True)
+    train_loader = DataLoader(trainset, batch_size=5, shuffle=True, drop_last=True)
+    valid_loader = DataLoader(validset, batch_size=5, shuffle=True, drop_last=True)
     return train_loader, valid_loader
 
 
 if __name__ == '__main__':
-    dataset = LearnDataset()
-    print(dataset[0])
+    # dataset = LearnDataset()
+    # print(dataset[0])
     train, valid = get_dataloader()
-    for idx, (x, y) in enumerate(train):
-        print(x, y)
+    for i in range(2):
+        print("=====================================")
+        for idx, (x, y) in enumerate(train):
+            print(x)
+            print(y)
+            output = torch.LongTensor([[0,1],[0,1],[0,1],[1,0],[1,0]])
+            print(torch.mean((output.argmax(1) == y).float()).item())
+        for idx, (x, y) in enumerate(valid):
+            print(x, y)
