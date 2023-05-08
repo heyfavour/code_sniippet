ec74eeb42f5f6153736ffe7c537264ed300183b6
heyfavourite <619511821@qq.com>
2023-05-08 15:53:00 +0800

    torch

--- pytorch/gradient_descent/gradient_descent_nn.py
+++ pytorch/gradient_descent/gradient_descent_nn.py
@@ -12,7 +12,7 @@ def init_weight(layer):
         # layer.bias.data = torch.zeros(layer.bias.data.size())
 
 
-class Demo(nn.Module):
+class LinearModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer = nn.Linear(1, 1)
@@ -26,27 +26,19 @@ class Demo(nn.Module):
 if __name__ == '__main__':
     x = torch.Tensor([i for i in range(1, 5)]).reshape(-1,1)
     y = torch.Tensor([i * 2 + 2 for i in range(1, 5)]).reshape(-1,1)
-    model = Demo()
+    model = LinearModel()
+    criterion = nn.MSELoss()#size_average 求均值 reduce 求和降维
     optimizer = Adam(model.parameters(), lr=0.01)
-    criterion = nn.MSELoss()
-    print(x)
-    print(y)
-    # print(model.parameters())
-    for epoch in range(2):
-        print(f"[epoch] [{epoch}]================================")
+    print(model.layer.weight.data)
+    for epoch in range(1000):
         predict = model(x)
-        optimizer.zero_grad()
         loss = criterion(predict,y)
-        print("loss",loss)
-        print(predict)
-        print("before *****************************")
-        print(model.layer.weight.grad,model.layer.bias.grad)
+        print(f"[epoch:{epoch}] [loss:{loss}]")
+        optimizer.zero_grad()
         loss.backward()
-        print("after *****************************")
-        #print(model.layer.weight,model.layer.bias)
-        print(model.layer.weight.grad,model.layer.bias.grad)
-        optimizer.step()
-        print("weight *****************************")
-        print(model.layer.weight,model.layer.bias)
+        print(model.layer.weight.data,model.layer.bias.data,model.layer.weight.grad.data)
+        optimizer.step()#update
+        print(model.layer.weight.data,model.layer.bias.data)
+
 
 
