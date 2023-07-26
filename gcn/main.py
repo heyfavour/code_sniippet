c165f2f67ae926889023bfaba2298a0d1f1d9ebe
mr.wang <619511821@qq.com>
2023-07-26 21:20:48 +0800

    GCN

--- gcn/main.py
+++ gcn/main.py
@@ -1,14 +1,11 @@
-"""
-邻接矩阵
-特征矩阵
-
-连接矩阵
-度矩阵
-
-D^(-1/2)*A*D^(-1/2)HW
-
-pip install torch-geometric
-"""
 import torch
+
+from torch import nn
 from torch_geometric.data import Data
 from torch_geometric.nn import GCNConv
+
+class GNN(nn.Module):
+    def __init__(self,features):
+        super().__init__()
+        self.gcn_1 = GCNConv(features,16)
+        self.gcn_1 = GCNConv(features,16)
