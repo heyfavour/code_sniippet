c6dd3150d4f7ca5897f6172615de462b285c71b6
mr.wang <619511821@qq.com>
2023-08-19 22:11:13 +0800

    混合精度

--- pytorch/torch_amp.py
+++ pytorch/torch_amp.py
@@ -11,11 +11,11 @@ scaler = torch.cuda.amp.GradScaler()
 if __name__ == '__main__':
     for epoch in range(100):
         model.train()
+        optimizer.zero_grad()
         with torch.cuda.amp.autocast():
             output = model()
             loss = criterion()
-        optimizer.zero_grad()
         scaler.scale(loss).backword()
 
         scaler.step(optimizer)
-        scaler.udpate()
+        scaler.update()
