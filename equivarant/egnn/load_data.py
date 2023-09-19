63a8071a60b54327810c96243140d471a35ac73d
wangzhenxiong <619511821@qq.com>
2023-09-19 16:40:40 +0800

    EGNN 学习

---
+++ equivarant/egnn/load_data.py
@@ -0,0 +1,13 @@
+from torch_geometric.datasets import QM9
+from torch_geometric.loader import DataLoader
+from torch.utils.data import random_split
+def QM9_dataloader():
+    dataset = QM9(root='./data')
+    train_dataset, valid_dataset = random_split(dataset, [0.9, 0.1])
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=512, shuffle=False)
+    return train_loader,valid_loader,len(train_dataset),len(valid_dataset)
+
+
+if __name__ == '__main__':
+    QM9_dataloader()
---
+++ equivarant/egnn/main.py
@@ -0,0 +1,67 @@
+import sys
+import datetime
+import random
+import numpy as np
+
+import torch
+import torch.optim as optim
+
+from model import EGNN
+from load_data import QM9_dataloader
+from schedules import LinearWarmupExponentialDecay
+from torch.utils.tensorboard import SummaryWriter
+
+
+def set_seed(seed=1):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+if __name__ == '__main__':
+    set_seed(99)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_loader, valid_loader, train_count, valid_count = QM9_dataloader()
+    model = EGNN(in_node_nf=15, in_edge_nf=0, hidden_nf=args.nf, device=device, n_layers=args.n_layers,
+                 coords_weight=1.0,
+                 attention=args.attention, node_attr=args.node_attr)
+    optimizer = optim.AdamW(model.parameters(), lr=0.001, amsgrad=True)
+    scheduler_lr = LinearWarmupExponentialDecay(optimizer, warmup_steps=3000, decay_rate=0.01, decay_steps=20000)  #
+    criterion = torch.nn.L1Loss()
+    writer = SummaryWriter("./logs")
+
+    model.train()
+    steps = 0
+    for epoch in range(500):
+        epoch_loss = 0
+        start_time = datetime.datetime.now()
+        for idx, data in enumerate(train_loader):
+            data.to(device)
+            optimizer.zero_grad()
+            out = model(data)
+            y = torch.reshape(data.y[:, 4], (-1, 1))
+            loss = criterion(out, y)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)
+            optimizer.step()
+            scheduler_lr.step()
+            epoch_loss += loss.item() * data.num_graphs
+            steps += 1
+            writer.add_scalar('lr', optimizer.param_groups[0]['lr'], steps)
+            sys.exit(0)
+        print(f"[EPOCH]:{epoch} loss:{epoch_loss / train_count}] lr:{optimizer.param_groups[0]['lr']}")
+        writer.add_scalar('train', epoch_loss / train_count, steps)
+        end_time = datetime.datetime.now()
+        use_time = end_time - start_time
+        model.eval()
+        with torch.no_grad():
+            epoch_loss = 0
+            for idx, data in enumerate(valid_loader):
+                data.to(device)
+                out = model(data)  # [bs,1]
+                y = torch.reshape(data.y[:, 4], (-1, 1))
+                loss = criterion(out, y)
+                epoch_loss += loss.item() * data.num_graphs
+        print(f"[VALID] loss:{epoch_loss / valid_count}]")
+        writer.add_scalar('valid', epoch_loss / valid_count, steps)
