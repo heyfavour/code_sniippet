bb94465661acbd212b9a50d34c8e12ade92b193d
wangzhenxiong <619511821@qq.com>
2023-09-21 17:59:26 +0800

    painn

---
+++ equivarant/painn/load_data.py
@@ -0,0 +1,29 @@
+import os
+import schnetpack as spk
+from schnetpack.datasets import QM9
+import schnetpack.transform as trn
+
+import torch
+import torchmetrics
+import pytorch_lightning as pl
+
+
+def get_dataloader():
+    qm9data = QM9(
+        './qm9.db',
+        batch_size=1,
+        num_train=110000,
+        num_val=10000,
+        transforms=[trn.ASENeighborList(cutoff=5.), trn.CastTo32()],
+        property_units={QM9.gap: 'eV'},
+        num_workers=1,
+        pin_memory=True,  # set to false, when not using a GPU
+        load_properties=[QM9.gap],  # only load U0 property
+    )
+    qm9data.prepare_data()
+    qm9data.setup()
+    return qm9data
+
+
+if __name__ == '__main__':
+    qm9data = get_dataloader()
